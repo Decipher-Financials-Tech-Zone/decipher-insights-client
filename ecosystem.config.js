b72afd3210f87ad8ac83ba5e3a0ba989db1d063f
Vaibhav Yadav <vaibhav.yadav@decipherfinancials.com>
2025-11-20 16:22:31 +0530

    Third commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: "decipher_frontend",
-      cwd: "/var/www/decipher_insights_frontend/current",
+      name: "decipher_insights_client",
+      cwd: "/var/www/decipher_insights_client",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3020",
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
       },
       instances: 1,
       exec_mode: "fork",
-      out_file: "/var/www/decipher_insights_frontend/logs/out.log",
-      error_file: "/var/www/decipher_insights_frontend/logs/err.log",
+      out_file: "/var/www/decipher_insights_client/logs/out.log",
+      error_file: "/var/www/decipher_insights_client/logs/err.log",
       merge_logs: true,
-      max_memory_restart: "512M"
-    }
-  ]
-}
+      max_memory_restart: "512M",
+    },
+  ],
+};
