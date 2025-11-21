19246c8483e28d5c7b35b2b85973d4ebd96c6417
Vaibhav Yadav <vaibhav.yadav@decipherfinancials.com>
2025-11-21 12:20:03 +0530

    pipeline

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "decipher_insights_client",
-      cwd: "/var/www/decipher_insights_client",
+      cwd: "/var/www/decipher-insights-client",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3020",
       env: {
@@ -10,8 +10,8 @@ module.exports = {
       },
       instances: 1,
       exec_mode: "fork",
-      out_file: "/var/www/decipher_insights_client/logs/out.log",
-      error_file: "/var/www/decipher_insights_client/logs/err.log",
+      out_file: "/var/www/decipher-insights-client/logs/out.log",
+      error_file: "/var/www/decipher-insights-client/logs/err.log",
       merge_logs: true,
       max_memory_restart: "512M",
     },
